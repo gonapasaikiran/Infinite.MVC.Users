2e8a206f42f15b6b54af6cfc9e03a40ecced418e
saikirango <saikirango@ICS-LAP-2285>
2023-01-30 23:11:59 +0530

    Add project files.

---
+++ Infinite.MVC.Users/Controllers/AdmissionFormController.cs
@@ -0,0 +1,166 @@
+﻿using Infinite.MVC.Users.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Infinite.MVC.Users.Controllers
+{
+    public class AdmissionFormController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        public AdmissionFormController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<FormViewModel> forms = new();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new System.Uri(_configuration["ApiUrl:api"]);
+                var result = await client.GetAsync("Admissions/GetAllForms");
+                if (result.IsSuccessStatusCode)
+                {
+                    forms = await result.Content.ReadAsAsync<List<FormViewModel>>();
+                }
+
+            }
+            return View(forms);
+        }
+        [Route("AdmissionForm/Details/{id}")]
+        [HttpGet]
+        
+        public async Task<IActionResult> Details(int id)
+        {
+
+            FormViewModel form = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(_configuration["ApiUrl:api"]);
+                var result = await client.GetAsync($"Admissions/GetFormsById/{id}");
+                if (result.IsSuccessStatusCode)
+                {
+                    form = await result.Content.ReadAsAsync<FormViewModel>();
+                    
+                }
+            }
+            return View(form);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            FormViewModel form = await this.FormById(id);
+            if (form != null)
+            {
+                return View(form);
+            }
+            ModelState.AddModelError("", "Server Error. Please try later");
+            return View(form);
+        }
+
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(FormViewModel form)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(_configuration["ApiUrl:api"]);
+                    var result = await client.PostAsJsonAsync("Admissions/CreateForm", form);
+                    if (result.StatusCode == System.Net.HttpStatusCode.Created)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                }
+            }
+            return View();
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> Edit(int Id)
+        {
+            
+                FormViewModel form = null;
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(_configuration["ApiUrl:api"]);
+                    var result = await client.GetAsync($"Admissions/GetFormsById/{Id}");
+                    if (result.IsSuccessStatusCode)
+                    {
+                        form = await result.Content.ReadAsAsync<FormViewModel>();
+                        return View(form);
+                    }
+                    
+                
+            }
+            return View();
+        }
+
+        [HttpPost("AdmissionForm/Edit/{Id}")]
+
+        public async Task<IActionResult> Edit(FormViewModel form)
+        {
+            if (!ModelState.IsValid)
+            {
+                using (var client = new HttpClient())
+                {
+                    //client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", HttpContext.Session.GetString("token"));
+                    client.BaseAddress = new Uri(_configuration["ApiUrl:api"]);
+                    var result = await client.PutAsJsonAsync($"Admissions/UpdateForm/{form.Id}", form);
+                    if (result.StatusCode == System.Net.HttpStatusCode.NoContent)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                }
+            }
+            return View();
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Delete(FormViewModel form)
+        {
+            using (var client = new HttpClient())
+            {
+                //client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", HttpContext.Session.GetString("token"));
+                client.BaseAddress = new System.Uri(_configuration["ApiUrl:api"]);
+                var result = await client.DeleteAsync($"Admissions/DeleteForm/{form.Id}");
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }   
+            return View();
+        }
+        [NonAction]
+        public async Task<FormViewModel> FormById(int id)
+        {
+            FormViewModel form = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new System.Uri(_configuration["ApiUrl:api"]);
+                var result = await client.GetAsync($"Admissions/GetFormsById/{id}");
+                if (result.IsSuccessStatusCode)
+                {
+                    form = await result.Content.ReadAsAsync<FormViewModel>();
+                }
+            }
+            return form;
+        }
+    }
+}
+
---
+++ Infinite.MVC.Users/Models/FormViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Infinite.MVC.Users.Models
+{
+    public class FormViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Please Provide FirstName")]
+        [Display(Name ="First Name")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Please Provide LastName")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Please Provide FatherName")]
+        [Display(Name = "Father Name")]
+        public string FatherName { get; set; }
+        [Required(ErrorMessage = "Please Provide MotherName")]
+        [Display(Name = "Mother Name")]
+        public string MotherName { get; set; }
+        [Required(ErrorMessage = "Please Provide Valid EmailID")]
+       
+        public string EmailId { get; set; }
+        [Required(ErrorMessage = "Please Provide Valid Mobile Number")]
+     
+        public string PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Please Provide Gender")]
+      
+        public string Gender { get; set; }
+        [Required(ErrorMessage = "Please Provide Date Of Birth")]
+        public DateTime DateOfBirth { get; set; }
+        [Required(ErrorMessage = "Please Provide Address")]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "Please Provide City")]
+        public string City { get; set; }
+        [Required(ErrorMessage = "Please Provide State")]
+        public string State { get; set; }
+        [Required(ErrorMessage = "Please Provide Qualification")]
+        public string Qualification { get; set; }
+        [Required(ErrorMessage = "Please Provide EntranceTestScore")]
+        public double EntranceTestScore { get; set; }
+    }
+}
